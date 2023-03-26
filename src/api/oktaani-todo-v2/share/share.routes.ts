b2e26a50c079edbb02ad4285551fa4806af80629
Epikle <98042337+Epikle@users.noreply.github.com>
2023-03-26 13:23:50 +0300

    New sharing todo api

--- src/api/oktaani-todo-v2/share/share.routes.ts
+++ src/api/oktaani-todo-v2/share/share.routes.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 
+import {
+  createSharedCollection,
+  deleteSharedCollection,
+  getSharedCollection,
+} from './share.handlers';
+
 const router = express.Router();
 
 //api/v2/oktaani-todo-v2/share/
-router.get('/', (_req, res) => {
-  res.sendStatus(200);
-});
-// router.get('/:id', getShareById);
-// router.post('/', createNewShare);
-// router.delete('/:id', deleteShareById);
+router.get('/:id', getSharedCollection);
+router.post('/', createSharedCollection);
+router.delete('/:id', deleteSharedCollection);
 
 export default router;
