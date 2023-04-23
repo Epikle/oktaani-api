ed9266e43ae72aa7bdddb61486c019dfc230f44c
Epikle <98042337+Epikle@users.noreply.github.com>
2023-04-23 15:30:31 +0300

    :arrow_up: Dependencies and :bug:

--- src/api/oktaani-todo-v2/share/share.handlers.ts
+++ src/api/oktaani-todo-v2/share/share.handlers.ts
@@ -3,8 +3,13 @@ import { Request, Response } from 'express';
 import Share from './share.model';
 
 export const createSharedCollection = async (req: Request, res: Response) => {
-  const createdShare = new Share(req.body);
-  await createdShare.save();
+  const { id } = req.body;
+  const collection = await Share.findOne({ id }, '-_id -__v -todos._id');
+
+  if (!collection) {
+    const createdShare = new Share(req.body);
+    await createdShare.save();
+  }
 
   res.sendStatus(201);
 };
