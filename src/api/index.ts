7e44a7944a51acfc767c979211ddfa254b943b8b
Epikle <98042337+Epikle@users.noreply.github.com>
2023-08-17 19:45:33 +0300

    :skull: Delete old API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import qwiaPhotosRoutes from './qwia-photos';
-import oktaaniTodoRoutes from './oktaani-todo';
 import oktaaniDtcRoutes from './oktaani-dtc';
 import oktaaniTodoV2Routes from './oktaani-todo-v2';
 
@@ -9,7 +8,6 @@ const router = express.Router();
 
 //api/v2/
 router.use('/qwia-photos', qwiaPhotosRoutes);
-router.use('/oktaani-todo', oktaaniTodoRoutes);
 router.use('/oktaani-dtc', oktaaniDtcRoutes);
 router.use('/oktaani-todo-v2', oktaaniTodoV2Routes);
 
