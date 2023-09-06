719c71924448bccb845a4eb111b25d3b16efd081
Epikle <98042337+Epikle@users.noreply.github.com>
2023-09-06 15:33:06 +0300

    :chart_with_upwards_trend: Update stats aggregate

--- src/api/oktaani-todo-v2/stats/stats.handlers.ts
+++ src/api/oktaani-todo-v2/stats/stats.handlers.ts
@@ -10,9 +10,36 @@ export const getStats = async (req: Request, res: Response) => {
     throw new Error('Bad Request');
   }
 
-  const statsCount = await Stats.find({ type }).countDocuments();
+  const sixMonthsAgo = new Date();
+  sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
 
-  res.json({ count: statsCount });
+  const lastSixMonths = await Stats.aggregate([
+    {
+      $match: {
+        type,
+        createdAt: { $gte: sixMonthsAgo },
+      },
+    },
+    {
+      $group: {
+        _id: { $month: '$createdAt' },
+        point: { $sum: 1 },
+      },
+    },
+    {
+      $sort: {
+        _id: 1,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        point: 1,
+      },
+    },
+  ]);
+
+  res.json(lastSixMonths);
 };
 
 export const createStats = async (req: Request, res: Response) => {
