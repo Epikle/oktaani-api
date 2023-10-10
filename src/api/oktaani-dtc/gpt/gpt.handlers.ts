25bb84f2938ed79f38fb586f736c98da044cb0b4
Epikle <98042337+Epikle@users.noreply.github.com>
2023-10-10 14:04:54 +0300

    :recycle: Update openai prompt

--- src/api/oktaani-dtc/gpt/gpt.handlers.ts
+++ src/api/oktaani-dtc/gpt/gpt.handlers.ts
@@ -1,18 +1,26 @@
 import { Request, Response } from 'express';
 import { openai } from './openai';
 
-function generatePrompt(code: string) {
-  return `Please provide a brief explanation for the car diagnostic trouble code listed after "Code:" at the end. 
-  If the code ends with an alphabet character, please disregard it. 
-  If you are unsure of its meaning or if the code does not conform to a proper diagnostic trouble code, 
-  kindly respond with 'no explanation found' instead of attempting to provide an inaccurate response. 
-  Code: ${code}`;
+function generatePrompt({ code, description }: { code: string; description: string }) {
+  return `Please provide a brief explanation for the car diagnostic trouble code listed after 'Code:' at the end. 
+  A short description is provided for more context after 'Description:'.
+
+  If the code ends with an alphabet character, please disregard it.
+  If you are unsure of the meaning of the code or if the code does not conform to a proper diagnostic trouble code, 
+  kindly respond with 'no explanation found' instead of attempting to provide an inaccurate response.
+
+  Format your response as follows: 'Code [inser given Code here], [insert response here]'.
+
+  Code: ${code}
+  Description: ${description}`;
 }
 
 export async function getCompletion(req: Request, res: Response) {
-  const { codeTitle } = req.query;
+  const { code, description } = req.query;
 
-  if (!codeTitle || typeof codeTitle !== 'string') return res.status(400).json({ message: 'Invalid request.' });
+  if (!code || typeof code !== 'string' || !description || typeof description !== 'string') {
+    return res.status(400).json({ message: 'Invalid request.' });
+  }
 
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
@@ -21,7 +29,7 @@ export async function getCompletion(req: Request, res: Response) {
     messages: [
       {
         role: 'user',
-        content: generatePrompt(codeTitle),
+        content: generatePrompt({ code, description }),
       },
     ],
   });
